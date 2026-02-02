ceb2a1eac49ff13d7f4b32db264c115b7e703114
Charles <charles.d.trabalho@gmail.com>
2026-02-02 11:07:11 -0300

    feat(user): create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,8 @@
-export class User {}
+export class User {
+    private readonly id: string;
+    private readonly name: string;
+    private readonly email: string;
+    private readonly password: string;
+    private readonly createdAt: Date;
+    private readonly updatedAt: Date;
+}
