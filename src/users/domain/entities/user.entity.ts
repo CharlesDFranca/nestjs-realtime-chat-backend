6f88b05586dc97549f5d0cf9ab2abe55219848d5
Charles <charles.d.trabalho@gmail.com>
2026-02-03 20:56:31 -0300

    refactor(user): replace duplicated entity fields with base Entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,12 +1,10 @@
+import { Entity } from "@/shared/domain/entities/entity";
 import { Email } from "../value-objects/email.vo";
 import { Password } from "../value-objects/password.vo";
 import { Username } from "../value-objects/username.vo";
 
-export class User {
-    private readonly id: string;
+export class User extends Entity {
     private readonly name: Username;
     private readonly email: Email;
     private readonly password: Password;
-    private readonly createdAt: Date;
-    private readonly updatedAt: Date;
 }
