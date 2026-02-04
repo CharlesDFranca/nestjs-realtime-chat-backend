95f6ad8813593fc34e1c45385e9c1fd95c544821
Charles <charles.d.trabalho@gmail.com>
2026-02-03 22:30:51 -0300

    feat(user): expose user properties via getters

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -4,7 +4,19 @@ import { Password } from "../value-objects/password.vo";
 import { Username } from "../value-objects/username.vo";
 
 export class User extends Entity {
-    private readonly name: Username;
-    private readonly email: Email;
-    private readonly password: Password;
+    private readonly _name: Username;
+    private readonly _email: Email;
+    private readonly _password: Password;
+
+    public get name(): Username {
+        return this._name;
+    }
+
+    public get email(): Email {
+        return this._email;
+    }
+
+    public get password(): Password {
+        return this._password;
+    }
 }
