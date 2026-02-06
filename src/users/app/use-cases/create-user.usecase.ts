e2db8c4c89e372d5005e6d9424a4ac80a277f482
Charles Dias <josecharlestill@gmail.com>
2026-02-05 22:57:59 -0300

    feat(user): create CreateUser usecase

---
+++ src/users/app/use-cases/create-user.usecase.ts
@@ -0,0 +1,47 @@
+import { IUseCase } from "@/shared/app/contracts/use-case";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { failure, Result, success } from "@/shared/app/results/result";
+import { IUserRepository } from "@/users/domain/repositories/user-repository";
+import { Email } from "@/users/domain/value-objects/email.vo";
+import { ApplicationError } from "@/shared/app/errors/application.error";
+import { User } from "@/users/domain/entities/user.entity";
+import { IdGenerator } from "@/shared/app/contracts/id-generator";
+import { EmailAlreadyUsedError } from "../errors/email-already-used.error";
+
+type CreateUserInput = CreateUserDto;
+type CreateUserOutput = { id: string };
+
+export class CreateUserUseCase implements IUseCase<
+    CreateUserInput,
+    CreateUserOutput
+> {
+    constructor(
+        private readonly userRepository: IUserRepository,
+        private readonly idGenerator: IdGenerator,
+    ) {}
+
+    async execute(input: CreateUserInput): Promise<Result<CreateUserOutput>> {
+        try {
+            await this.ensureEmailIsNotUsed(input.email);
+
+            const user = User.create(this.idGenerator.generate(), input);
+
+            await this.userRepository.save(user);
+
+            return success({ id: user.id });
+        } catch (e: unknown) {
+            if (e instanceof ApplicationError) {
+                return failure(e);
+            }
+
+            throw e;
+        }
+    }
+
+    private async ensureEmailIsNotUsed(raw: string) {
+        const email = Email.create(raw);
+        const alreadyUsed = await this.userRepository.findByEmail(email);
+
+        if (alreadyUsed) throw new EmailAlreadyUsedError(email.value);
+    }
+}
